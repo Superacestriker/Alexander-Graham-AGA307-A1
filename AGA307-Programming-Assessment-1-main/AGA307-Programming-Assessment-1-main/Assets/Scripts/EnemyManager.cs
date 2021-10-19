8de487e6befd4147a55ee9fce2d1139bcd12cf10
Superacestriker <91574721+Superacestriker@users.noreply.github.com>
2021-10-20 00:15:27 +1000

    week 3 fixes
    did the requirements but sadly not the bonus stuff. Need to do more there.

--- AGA307-Programming-Assessment-1-main/AGA307-Programming-Assessment-1-main/Assets/Scripts/EnemyManager.cs
+++ AGA307-Programming-Assessment-1-main/AGA307-Programming-Assessment-1-main/Assets/Scripts/EnemyManager.cs
@@ -13,17 +13,29 @@ public class EnemyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		for (int i = 0; i < spawnPoints.Length; i++)
+        /*
+        for (int i = 0; i < spawnPoints.Length; i++)
 		{
             int rNum = Random.Range(0, enemyTypes.Length);
             GameObject e = Instantiate(enemyTypes[rNum], spawnPoints[i].position, spawnPoints[i].rotation);
             enemies.Add(e);
         }
+        */
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+            TargetSpawn();
+    }
+
+    void TargetSpawn()
+    {
+        int rEnemy = Random.Range(0, enemyTypes.Length);
+        int rSpawn = Random.Range(0, spawnPoints.Length);
+        GameObject e = Instantiate(enemyTypes[rEnemy], spawnPoints[rSpawn].position, spawnPoints[rSpawn].rotation);
+        enemies.Add(e);
     }
 }
--- AGA307-Programming-Assessment-1-main/AGA307-Programming-Assessment-1-main/Assets/Scripts/Target.cs
+++ AGA307-Programming-Assessment-1-main/AGA307-Programming-Assessment-1-main/Assets/Scripts/Target.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    int health = 3;
+    public int health = 3;
     // Start is called before the first frame update
     void Start()
     {
